67ef49e1a24dc22a743042ac087b4b942c04fa2e
核心工作室-TWB <102907913+corestudi0@users.noreply.github.com>
2022-11-15 08:15:09 +0800

    Update time.js

--- mc/home/static/js/time.js
+++ mc/home/static/js/time.js
@@ -12,7 +12,7 @@ function time() {
     var todayHour = today.getHours()
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
-    var t1 = Date.UTC(2022, 8, 25, 00, 00, 00)
+    var t1 = Date.UTC(2022, 8, 22, 19, 40, 00)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
     var diffYears = Math.floor(diff / years)
